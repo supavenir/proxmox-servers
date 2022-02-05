b0d273bc737072fbc6fe5a84b45d2bfc432b1aa8
ThomasL <thomasleconte05@gmail.com>
2022-02-05 19:41:54 +0100

    Feat: Ajout d'un serveur

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -2,9 +2,16 @@
 
 namespace controllers;
 
+use models\Serveur;
+use Ubiquity\attributes\items\router\Post;
+use Ubiquity\attributes\items\router\Get;
+
 use Ajax\php\ubiquity\JsUtils;
-use PHPMV\ProxmoxApi;
 use PHPMV\ProxmoxMaster;
+use Ubiquity\controllers\Router;
+use Ubiquity\orm\DAO;
+use Ubiquity\utils\http\URequest;
+use Ubiquity\utils\http\UResponse;
 
 /**
  * Controller AdminController
@@ -12,27 +19,72 @@ use PHPMV\ProxmoxMaster;
  */
 class AdminController extends \controllers\ControllerBase
 {
-
+    #[Get(path: "/server/vms", name: "admin.getVms")]
     public function index()
     {
-        $api = new ProxmoxApi('servers1.sts-sio-caen.info', 'sio1a', 'sio1a');
-        $vms = $api->getVMs();
+        $vms = $this->api->getVMs();
         $dt = $this->jquery->semantic()->dataTable('vms', \StdClass::class, $vms);
         $dt->setFields(ProxmoxMaster::VM_FIELDS);
-        $dt->fieldAsLabel('status', ['jsCallback' => function ($lbl, $instance) {
-            if ($instance->status === 'running') {
-                $lbl->addClass('green');
+        $dt->fieldAsLabel('status', attributes: ['jsCallback' => function ($label, $instance) {
+            if ($instance->status == "running") {
+                $label->addClass('green');
+            }
+            if ($instance->status == "stopped") {
+                $label->addClass('red');
             }
         }]);
-        $this->makeInitialTemplate();
         $this->jquery->renderDefaultView();
 
         /*$this->loadView("AdminController/index.html");*/
     }
 
-    public function makeInitialTemplate(){
-        $div = $this->jquery->semantic()->htmlDivider('burger-menu');
-        $menu = $this->jquery->semantic()->htmlAccordionMenu('menu', ["Serveurs", "VMs"]);
-        $div->addContent($menu);
+    #[Get(path: "/server/add", name: "admin.addServer")]
+    public function addServer()
+    {
+        $form = $this->jquery->semantic()->htmlForm("addServerForm", [
+            $this->jquery->semantic()->htmlInput("server-name", "text", null, "Nom du serveur"),
+            $this->jquery->semantic()->htmlInput("server-ip", "text", null, "IP du serveur"),
+            $this->jquery->semantic()->htmlInput("server-login", "text", null, "Identifiant"),
+            $this->jquery->semantic()->htmlInput("server-pwd", "password", null, "Mot de passe"),
+        ]);
+        $form->setProperty("method", "POST");
+        $form->setProperty("action", Router::path("admin.postAddServer"));
+        $form->addExtraFieldRule("server-name", "empty");
+        $form->addExtraFieldRule("server-ip", "empty");
+        $form->addExtraFieldRule("server-login", "empty");
+        $form->addExtraFieldRule("server-pwd", "empty");
+/*        $this->jquery->jsonOn('change', '#server-name', Router::path('admin.getServerIpByName', []),
+            'post', ['attr' => 'value', "jsCallback" => function ($response) {
+                $serverIp = $this->jquery->semantic()->getHtmlComponent("server-ip");
+                $serverIp->setProperty("value", $response);
+            }]);*/
+        $form->addButton("form-submit", "Valider");
+        $this->jquery->renderView("AdminController/addServer.html");
     }
+
+    /**
+     * @throws \Ubiquity\exceptions\RouterException
+     */
+    #[Post(path: "/server/add", name: "admin.postAddServer")]
+    public function saveServer()
+    {
+        $values = URequest::getPost();
+        $serveur = new Serveur();
+        $serveur->setDnsName($values["server-name"]);
+        $serveur->setIpAddress($values["server-ip"]);
+        $serveur->setLogin($values["server-login"]);
+        $serveur->setPassword($values["server-pwd"]);
+        DAO::save($serveur);
+        $this->forward("controllers\AdminController", "index", null, true, true);
+    }
+
+
+    #[Post(path: "/server/ip/{name}", name: "admin.getServerIpByName")]
+    public function getServerIpByNamee($name)
+    {
+        UResponse::asJSON();
+        $name = trim($name);
+        echo json_encode(["ip" => empty($name) ? "" : gethostbyname($name)]);
+    }
+
 }
--- app/controllers/ControllerBase.php
+++ app/controllers/ControllerBase.php
@@ -1,28 +1,51 @@
 <?php
+
 namespace controllers;
 
-use Ubiquity\controllers\Controller;
+use Ajax\php\ubiquity\JsUtils;
+use models\Serveur;
+use PHPMV\ProxmoxApi;
+use Ubiquity\orm\DAO;
 use Ubiquity\utils\http\URequest;
+use Ubiquity\controllers\Controller;
 
 /**
  * controllers$ControllerBase
+ * @property JsUtils jquery
  */
-abstract class ControllerBase extends Controller {
+abstract class ControllerBase extends Controller
+{
 
-	protected $headerView = "@activeTheme/main/vHeader.html";
+    protected $headerView = "@activeTheme/main/vHeader.html";
 
-	protected $footerView = "@activeTheme/main/vFooter.html";
+    protected $footerView = "@activeTheme/main/vFooter.html";
 
-	public function initialize() {
-		if (! URequest::isAjax()) {
-			$this->loadView($this->headerView);
-		}
-	}
+    protected $api;
 
-	public function finalize() {
-		if (! URequest::isAjax()) {
-			$this->loadView($this->footerView);
-		}
-	}
+    public function initialize()
+    {
+        $this->api = new ProxmoxApi('servers1.sts-sio-caen.info', 'sio1a', 'sio1a');
+        if (!URequest::isAjax()) {
+            $this->loadView($this->headerView);
+        }
+        $this->makeInitialTemplate();
+    }
+
+    public function finalize()
+    {
+        if (!URequest::isAjax()) {
+            $this->loadView($this->footerView);
+        }
+    }
+
+    public function makeInitialTemplate()
+    {
+        $div = $this->jquery->semantic()->htmlDivider('burger-menu');
+        $menu = $this->jquery->semantic()->htmlAccordionMenu('menu', ["Serveurs", "VMs"]);
+        $serveurs = DAO::getAll(Serveur::class);
+        $serveurs = array_map(fn($serveur): string => $serveur->getDnsName(), $serveurs);
+        $menu->getItem(0)->addContent($this->jquery->semantic()->htmlList("servers", $serveurs));
+        $div->addContent($menu);
+    }
 }
 
